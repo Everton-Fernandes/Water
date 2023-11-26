75f5f80ea3a69cfe311e5b5f5c5ee1f906604c0c
drout0 <lawrencebr@hotmail.com>
2023-11-26 18:16:30 -0300

    WIP

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import * as THREE from "https://cdn.skypack.dev/three@0.133.1";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls.js";
 
 let scene, camera, renderer, controls;
-let waterBlocks = [];
-let grassBlocks = [];
+let blocks = [];
 let intervalId;
 const intervalTime = 1000;
 let waterCube;
@@ -34,26 +33,7 @@ function init() {
   controls.screenSpacePanning = false;
   controls.maxPolarAngle = Math.PI / 2;
 
-  drawGrass()
-
   window.addEventListener("resize", onWindowResize);
-}
-
-function drawGrass() {
-  for (let x = 5; x < 7; x++) {
-    for (let y = 5; y < 7; y++) {
-      const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-      const cubeMaterial = new THREE.MeshBasicMaterial({
-        color: 0x008000,
-        wireframe: false,
-      });
-      grassCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-      
-      grassCube.position.set(x, 0, y);
-      scene.add(grassCube);
-      grassBlocks.push(grassCube);
-    }
-  }
 }
 
 function startDrawing() {
@@ -67,46 +47,86 @@ function xyToIndex(x, y, width) {
 function WaterFunction(matrix, actives, width) {
   var newActives = [];
   for (var i = 0; i < actives.length; i++) {
-    if (actives[i].force > 0) {
-      var left = {
-        x: actives[i].x - 1,
-        y: actives[i].y,
-        force: actives[i].force - 1,
-      };
+    var rl = 0, rr = 0, rt = 0, rb = 0;
 
-      var right = {
-        x: actives[i].x + 1,
-        y: actives[i].y,
-        force: actives[i].force - 1,
-      };
+    if(matrix[xyToIndex(actives[i].x - 1, actives[i].y, width)] == GRASS) {
+      rl = 7 - (actives[i].level - 1);
+    }
 
-      var top = {
-        x: actives[i].x,
-        y: actives[i].y + 1,
-        force: actives[i].force - 1,
-      };
+    if(matrix[xyToIndex(actives[i].x + 1, actives[i].y, width)] == GRASS) {
+      rr = 7 - (actives[i].level - 1);
+    }
 
-      var bottom = {
-        x: actives[i].x,
-        y: actives[i].y - 1,
-        force: actives[i].force - 1,
-      };
+    if(matrix[xyToIndex(actives[i].x, actives[i].y + 1, width)] == GRASS) {
+      rt = 7 - (actives[i].level - 1);
+    }
 
+    if(matrix[xyToIndex(actives[i].x, actives[i].y - 1, width)] == GRASS) {
+      rb = 7 - (actives[i].level - 1);
+    }
+
+    var left = {
+      x: actives[i].x - 1,
+      y: actives[i].y,
+      left: actives[i].left - 1 - rl,
+      right: actives[i].right - 1 - rr,
+      top: actives[i].top - 1 - rt,
+      bottom: actives[i].bottom - 1 - rb,
+      level: actives[i].level - 1
+    };
+
+    var right = {
+      x: actives[i].x + 1,
+      y: actives[i].y,
+      left: actives[i].left - 1 - rl,
+      right: actives[i].right - 1 - rr,
+      top: actives[i].top - 1 - rt,
+      bottom: actives[i].bottom - 1 - rb,
+      level: actives[i].level - 1
+    };
+
+    var top = {
+      x: actives[i].x,
+      y: actives[i].y + 1,
+      left: actives[i].left - 1 - rl,
+      right: actives[i].right - 1 - rr,
+      top: actives[i].top - 1 - rt,
+      bottom: actives[i].bottom - 1 - rb,
+      level: actives[i].level - 1
+    };
+
+    var bottom = {
+      x: actives[i].x,
+      y: actives[i].y - 1,
+      left: actives[i].left - 1 - rl,
+      right: actives[i].right - 1 - rr,
+      top: actives[i].top - 1 - rt,
+      bottom: actives[i].bottom - 1 - rb,
+      level: actives[i].level - 1
+    };
+
+    if (actives[i].left > 0) {
       if (matrix[xyToIndex(left.x, left.y, width)] == 0) {
         matrix[xyToIndex(left.x, left.y, width)] = 1;
         newActives.push(left);
       }
+    }
 
+    if (actives[i].right > 0) {
       if (matrix[xyToIndex(right.x, right.y, width)] == 0) {
         matrix[xyToIndex(right.x, right.y, width)] = 1;
         newActives.push(right);
       }
+    }
 
+    if (actives[i].top > 0) {
       if (matrix[xyToIndex(top.x, top.y, width)] == 0) {
         matrix[xyToIndex(top.x, top.y, width)] = 1;
         newActives.push(top);
       }
+    }
 
+    if (actives[i].bottom > 0) {
       if (matrix[xyToIndex(bottom.x, bottom.y, width)] == 0) {
         matrix[xyToIndex(bottom.x, bottom.y, width)] = 1;
         newActives.push(bottom);
@@ -118,42 +138,57 @@ function WaterFunction(matrix, actives, width) {
 }
 
 var width = 15;
-var actives = [{ x: 7, y: 7, force: 7 }];
+var actives = [{ x: 7, y: 7, left: 7, right: 7, top: 7, bottom: 7, level: 7 }];
 var matrix = new Array(width * width);
 
 for (var i = 0; i < width * width; i++) {
   matrix[i] = 0;
 }
 
-matrix[xyToIndex(7, 7, width)] = 8; // Valor inicial de força de escoamento (máximo)
+const WATER = 1;
+const GRASS = 2;
+
+matrix[xyToIndex(7, 7, width)] = WATER;
+
+// matrix[xyToIndex(5, 8, width)] = GRASS;
+matrix[xyToIndex(6, 8, width)] = GRASS;
+matrix[xyToIndex(7, 8, width)] = GRASS;
+matrix[xyToIndex(8, 8, width)] = GRASS;
+// matrix[xyToIndex(9, 8, width)] = GRASS;
 
 function drawWater() {
   actives = WaterFunction(matrix, actives, width);
 
-  for (var i = 0; i < waterBlocks.length; i++) {
-    scene.remove(waterBlocks[i]);
+  for (var i = 0; i < blocks.length; i++) {
+    scene.remove(blocks[i]);
   }
 
-  waterBlocks = [];
+  blocks = [];
 
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < width; y++) {
       const index = xyToIndex(x, y, width);
-      if (matrix[index] > 0) {
+      const type = matrix[index];
+      if (type == WATER) {
         const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
         const cubeMaterial = new THREE.MeshBasicMaterial({
           color: 0x3399ff,
           wireframe: true,
         });
         waterCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-        
         waterCube.position.set(x, 0, y);
-        if(x == 5 && y == 5 || x == 5 && y == 6 || x == 6 && y == 5 || x == 6 && y == 6){
-          waterBlocks.pop(waterCube);
-        }else{
-          scene.add(waterCube);
-          waterBlocks.push(waterCube);
-        }
+        scene.add(waterCube);
+        blocks.push(waterCube);
+      } else if(type == GRASS) {
+        const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+        const cubeMaterial = new THREE.MeshBasicMaterial({
+          color: 0x99ff33,
+          wireframe: false,
+        });
+        waterCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+        waterCube.position.set(x, 0, y);
+        scene.add(waterCube);
+        blocks.push(waterCube);
       }
     }
   }
